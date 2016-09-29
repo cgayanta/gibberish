f8f1f6e8fc452a2ad74ba79883c9c5c8ca425f34
cgayanta <annakotelnikova@Annas-MacBook-Pro-2.local>
2016-09-29 15:22:34 -0400

    Project files

---
+++ main.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<h1>My First Heading</h1>
+
+<p>My first paragraph.</p>
+
+</body>
+
+<script src="https://www.gstatic.com/firebasejs/3.4.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBSY033ruhl_KULv0WYapj9v9E94njWKTc",
+    authDomain: "slack-emu.firebaseapp.com",
+    databaseURL: "https://slack-emu.firebaseio.com",
+    storageBucket: "slack-emu.appspot.com",
+    messagingSenderId: "828849344766"
+  };
+  firebase.initializeApp(config);
+</script>
+
+</html>
